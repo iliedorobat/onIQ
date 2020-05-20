16087921e90d0eea61bea8345e04af9230d0f613
Ilie Cristian Dorobat <idorobat@cisco.com>
2020-05-20 23:38:16 +0300

    add the language detector

--- src/ro/webdata/nqi/index.py
+++ src/ro/webdata/nqi/index.py
@@ -1,5 +1,7 @@
+import logging
 import ro.webdata.nqi.nlp.sparql as sparql
 import ro.webdata.nqi.rdf.graph_processor as graph
+from langdetect import detect
 
 
 ENDPOINT = "http://localhost:7200/repositories/eCHM"
@@ -7,6 +9,13 @@ QUERY = 'Display the artefacts hosted in Romania'
 SHOULD_PRINT = True
 
 # https://blog.einstein.ai/how-to-talk-to-your-database/
+
+if detect(QUERY) != "en":
+    logging.warning(
+        f'\n\tLanguage detected: "{detect(QUERY)}"'
+        f'\n\tLanguage required: "en"'
+    )
+
 sparql_query = sparql.get_query(ENDPOINT, QUERY, SHOULD_PRINT)
 
 if SHOULD_PRINT:
