c433008d5f01ec57b1b5386ae32082f877627009
Ilie Cristian Dorobat <idorobat@cisco.com>
2020-07-13 00:27:57 +0300

    updating the index file

--- src/ro/webdata/nqi/index.py
+++ src/ro/webdata/nqi/index.py
@@ -1,14 +1,12 @@
-import ro.webdata.nqi.nlp.sparql as sparql
-import ro.webdata.nqi.rdf.parser as parser
+import ro.webdata.nqi.nlp.sparql.sparql as sparql
 
 from ro.webdata.nqi.common.print_utils import print_lang_warning
-from ro.webdata.nqi.rdf.Match import Match
 
 ENDPOINT = "http://localhost:7200/repositories/TESTING_BCU_CLUJ"
+ENDPOINT = "http://localhost:7200/repositories/eCHO"
 
-QUERY = 'when the artifact were issued?'
-QUERY = 'when the artifacts were published?'
-
+QUERY = 'Which paintings are located in Bacau?'
+QUERY = 'Which paintings and statues are located in Bacau?'
 print_lang_warning(QUERY)
 sparql_query = sparql.get_query(ENDPOINT, QUERY)
 
@@ -16,6 +14,10 @@ sparql_query = sparql.get_query(ENDPOINT, QUERY)
 # # https://blog.einstein.ai/how-to-talk-to-your-database/
 # parser.parse_rdf("../../../../files/input/rdf/demo_2.rdf")
 
-# properties = parser.get_properties(ENDPOINT)
-# match = Match('published', properties)
-# print(match)
+
+# from ro.webdata.nqi.rdf.Match import Match
+# from ro.webdata.nqi.rdf import parser
+# match = Match(ENDPOINT, 'location')
+# print('location:', match)
+
+
