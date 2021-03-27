01a2d58ea14a87f3ce96ec5466792303e7dfc623
Ilie Cristian Dorobat <idorobat@cisco.com>
2021-03-27 02:14:57 +0200

    Adding new test questions

--- src/ro/webdata/oniq/index.py
+++ src/ro/webdata/oniq/index.py
@@ -1,6 +1,6 @@
 import spacy
 
-from ro.webdata.oniq.common.constants import APP_MODE
+from ro.webdata.oniq.common.constants import GLOBAL_ENV, TEST_MODES
 from ro.webdata.oniq.model.sparql.MetaQuery import MetaQuery
 from ro.webdata.oniq.nlp.statements import get_statement_list
 from ro.webdata.oniq.test.dataset import pairs
@@ -14,9 +14,9 @@ nlp = spacy.load('../../../../lib/en_core_web_sm/en_core_web_sm-2.2.5')
 # nlp = spacy.load('../../../../lib/en_core_web_md/en_core_web_md-2.2.5')
 
 QUERY = 'Which paintings are not located in Bacau?'
-if APP_MODE.IS_TEST_MODE is False:
+if GLOBAL_ENV.TEST_MODE == TEST_MODES.DEFAULT:
     sparql_query = MetaQuery(ENDPOINT, QUERY)
-
+elif GLOBAL_ENV.TEST_MODE == TEST_MODES.LOCAL_TEST:
     document = nlp(QUERY)
     statements = get_statement_list(document)
     question_statement_test(document.text, statements, pairs)
--- src/ro/webdata/oniq/test/dataset.py
+++ src/ro/webdata/oniq/test/dataset.py
@@ -1025,5 +1025,120 @@ statement: {
 	conjunction: or,
 	related_phrases: a panel
 """
+    },
+    {
+        # [3]
+        "query": "When was anıtkabir built?",
+        "result": """
+statement: {
+	target_phrase: when,
+	action: {
+		dep: ROOT,
+		is_available: False,
+		neg: None,
+		verb: {
+			aux_vbs: [was],
+			acomp: None,
+			main_vb: built,
+			modal_vb: None
+		}
+	},
+	conjunction: None,
+	related_phrases: anıtkabir
+"""
+    },
+    {
+        # [1]
+        "query": "What is the federated state located in the Weimar Republic?",
+        "result": """
+statement: {
+	target_phrase: what,
+	action: {
+		dep: ROOT,
+		is_available: False,
+		neg: None,
+		verb: {
+			aux_vbs: [is],
+			acomp: None,
+			main_vb: None,
+			modal_vb: None
+		}
+	},
+	conjunction: None,
+	related_phrases: the federated state
+
+statement: {
+	target_phrase: the federated state,
+	action: {
+		dep: acl,
+		is_available: False,
+		neg: None,
+		verb: {
+			aux_vbs: None,
+			acomp: None,
+			main_vb: located,
+			modal_vb: None
+		}
+	},
+	conjunction: None,
+	related_phrases: in the Weimar Republic
+"""
+    },
+    {
+        "query": "Where is the museum located?",
+        "result": """
+statement: {
+	target_phrase: where,
+	action: {
+		dep: ROOT,
+		is_available: False,
+		neg: None,
+		verb: {
+			aux_vbs: [is],
+			acomp: None,
+			main_vb: located,
+			modal_vb: None
+		}
+	},
+	conjunction: None,
+	related_phrases: the museum
+"""
+    },
+    {
+        # [1]
+        "query": "Which is the state and country of the Watergate scandal?",
+        "result": """
+statement: {
+	target_phrase: which,
+	action: {
+		dep: ROOT,
+		is_available: False,
+		neg: None,
+		verb: {
+			aux_vbs: [is],
+			acomp: None,
+			main_vb: None,
+			modal_vb: None
+		}
+	},
+	conjunction: None,
+	related_phrases: the state
+
+statement: {
+	target_phrase: which,
+	action: {
+		dep: ROOT,
+		is_available: False,
+		neg: None,
+		verb: {
+			aux_vbs: [is],
+			acomp: None,
+			main_vb: None,
+			modal_vb: None
+		}
+	},
+	conjunction: and,
+	related_phrases: country of the Watergate scandal
+"""
     }
 ]
