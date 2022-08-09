865e21f51ab4268a80401912b2b5babddc9ebc94
Ilie Cristian Dorobat <idorobat@cisco.com>
2022-08-10 00:30:51 +0300

    Remove redundant code

--- src/ro/webdata/oniq/common/print_const.py
+++
@@ -1,9 +0,0 @@
-# https://godoc.org/github.com/whitedevops/colors
-class COLORS:
-    BLUE = "\033[34m"
-    CYAN = "\033[36m"
-    LIGHT_CYAN = "\033[96m"
-    LIGHT_YELLOW = "\033[93m"
-    LIGHT_RED = "\033[91m"
-    RED = "\033[31m"
-    RESET_ALL = '\033[0m'
