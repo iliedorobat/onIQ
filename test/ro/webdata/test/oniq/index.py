5da44ec8edf9ad5e7f04e57e2e185c20ee50e811
Ilie Cristian Dorobat <idorobat@cisco.com>
2023-05-11 02:27:29 +0300

    Update test index.py script

--- test/ro/webdata/test/oniq/index.py
+++ test/ro/webdata/test/oniq/index.py
@@ -1,8 +1,8 @@
-from ro.webdata.test.oniq.endpoint.common.test_CSVService import TestCSVService
-from ro.webdata.test.oniq.endpoint.common.translator.test_CSVTranslator import TestCSVTranslator, TestCSVBasicTranslator
-from ro.webdata.test.oniq.endpoint.common.translator.test_URITranslator import TestURITranslator
-from ro.webdata.test.oniq.endpoint.dbpedia.test_lookup import TestLookupService
-from ro.webdata.test.oniq.endpoint.dbpedia.test_query import TestDBpediaQueryService
+from test.ro.webdata.test.oniq.endpoint.common.test_CSVService import TestCSVService
+from test.ro.webdata.test.oniq.endpoint.common.translator.test_CSVTranslator import TestCSVTranslator, TestCSVBasicTranslator
+from test.ro.webdata.test.oniq.endpoint.common.translator.test_URITranslator import TestURITranslator
+from test.ro.webdata.test.oniq.endpoint.dbpedia.test_lookup import TestLookupService
+from test.ro.webdata.test.oniq.endpoint.dbpedia.test_query import TestDBpediaQueryService
 
 # common pkg tests
 TestURITranslator.run()
